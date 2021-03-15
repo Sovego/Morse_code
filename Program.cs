dc1fd67d523edf33030a1ed19bacb9554b14d72e
Sovego <egor.sofronov03@gmail.com>
2021-03-15 23:32:53 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Morse_translator;
+namespace Morse_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Morse Code ");
+            string str=Console.ReadLine();
+            Console.WriteLine(Morse.TranslateFromMorse(str));
+            
+        }
+    }
+}
